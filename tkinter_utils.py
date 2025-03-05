0875bd9773441e0c61bd2ce72cea47d6ff6c04e7
Lang Towl <langtowl@gmail.com>
2025-03-05 12:13:42 -0700

    blurr segmentation fixed, mean shift segmentation implemnted

--- tkinter_utils.py
+++ tkinter_utils.py
@@ -20,6 +20,7 @@ class WindowRenderer:
         print("Path to image2 exists." if os.path.exists(image2) else "Path to image2 does not exist.")
         print("Path to image3 exists.\n" if os.path.exists(image3) else "Path to image3 does not exist.\n")
 
+
         # Collect images from passed path
         self.image1 = Image.open(image1)
         self.image1_copy = Image.open(image1)
@@ -45,11 +46,11 @@ class WindowRenderer:
         self.otsu_button.grid(row = 3, column = 0, pady = 5)
 
         # Add blurr button
-        self.blurr_button = tk.Button(self.root, text = "Blurr Segmentation", command = self.mean_shift)
+        self.blurr_button = tk.Button(self.root, text = "Blurr Segmentation", command = self.blurr_segmentation)
         self.blurr_button.grid(row = 4, column = 0, pady = 5)
 
         # Add mean shift button
-        self.mean_shift_button = tk.Button(self.root, text = "Mean Shift Segmentation", command = self.otsu_threshold)
+        self.mean_shift_button = tk.Button(self.root, text = "Mean Shift Segmentation", command = self.mean_shift_segmentation)
         self.mean_shift_button.grid(row = 5, column = 0, pady = 5)
 
         # Add segment button
@@ -68,7 +69,6 @@ class WindowRenderer:
 
         # Start main event loop
         self.root.mainloop()
-
 
     # Update images in canvas
     def update_canvas(self):
@@ -103,6 +103,9 @@ class WindowRenderer:
         image2 = np.array(self.image2)
         image3 = np.array(self.image3)
 
+        # Remove alpha channel from image 3
+        image3 = image3[:,:,:3]
+
         # Convert to grayscale
         image1_gs = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
         image2_gs = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
@@ -131,19 +134,52 @@ class WindowRenderer:
         # Redraw canvas with new images
         self.update_canvas()
 
-    def mean_shift(self):
+    def blurr_segmentation(self):
         print("Segmenting Images...")
 
         # Ready images for processing
-        image1, image2, _, _, _, _ = self.preprocess_images()
+        image1, image2, image3, _, _, _ = self.preprocess_images()
 
         # Apply mean shift filtering
         msf1 = cv2.pyrMeanShiftFiltering(image1, 20, 40)
         msf2 = cv2.pyrMeanShiftFiltering(image2, 20, 40)
+        msf3 = cv2.pyrMeanShiftFiltering(image3, 20, 40)
 
         # # Update images
         self.image1_copy = Image.fromarray(msf1)
         self.image2_copy = Image.fromarray(msf2)
+        self.image3_copy = Image.fromarray(msf3)
+
+        print("Done.\n")
+
+        # Redraw canvas with new images
+        self.update_canvas()
+
+    def mean_shift_segmentation(self):
+        print("Segmenting Images...")
+
+        # Ready images for processing
+        image1, image2, image3, _, _, _ = self.preprocess_images()
+
+        # Apply mean shift filtering
+        msf1 = cv2.pyrMeanShiftFiltering(image1, 20, 40)
+        msf2 = cv2.pyrMeanShiftFiltering(image2, 20, 40)
+        msf3 = cv2.pyrMeanShiftFiltering(image3, 20, 40)
+
+        # Convert blurred images to greyscale
+        msfb1 = cv2.cvtColor(msf1, cv2.COLOR_BGR2GRAY)
+        msfb2 = cv2.cvtColor(msf2, cv2.COLOR_BGR2GRAY)
+        msfb3 = cv2.cvtColor(msf3, cv2.COLOR_BGR2GRAY)
+
+        # Compute threshold
+        _, threshold1 = cv2.threshold(msfb1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        _, threshold2 = cv2.threshold(msfb2, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        _, threshold3 = cv2.threshold(msfb3, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        # # Update images
+        self.image1_copy = Image.fromarray(threshold1)
+        self.image2_copy = Image.fromarray(threshold2)
+        self.image3_copy = Image.fromarray(threshold3)
 
         print("Done.\n")
 
